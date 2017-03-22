292a153a42857c332b886ea58c91cb9c52bbf02c
mnb546 <rainer.lanemann@khk.ee>
2017-03-22 12:43:59 +0200

    Faili laadimise funktsioon

--- classes/template.php
+++ classes/template.php
@@ -13,10 +13,32 @@ class template
     var $content = false; //html malli faili sisu
     var $vars = array(); //html vaate sisu - reaalsed väärtused
     // klassi tegevused - meetodid - funktsioonid
+
+    //html malli faili lugemine
+    function loadFile(){
+        // lokaalne asendus
+        $f = $this->file;
+        // kontrollime mallide kausta olemasolu
+        if(!is_dir(TMP_DIR)){
+            echo 'Kataloogi '.TMP_DIR.' ei ole leitud<br />';
+            exit;
+        }
+        // Kui fail on olemas ja loetav
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            // loeme failist malli sisu
+            $this->readFile($f);
+        }
+        // Kui sisu ei ole võimalik lugeda
+        if($this->content === false) {
+            echo 'Ei suutnud lugeda faili '.$this->file.'<br />';
+        }
+    }
+
     // Loeme sisu html failist
     function readFile($f) {
         $this->content = file_get_contents($f);
     }
+
 }
 
 ?>
