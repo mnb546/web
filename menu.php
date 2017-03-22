85d37849a4ce9818958493a4161fa07e3e123b80
mnb546 <rainer.lanemann@khk.ee>
2017-03-22 15:30:24 +0200

    Kontrollime objekti olemasolu sisu

---
+++ menu.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainer.lanemann
+ * Date: 22.03.2017
+ * Time: 15:26
+ */
+// Loome menüü mallide objektid
+$menu = new template('menu.menu');
+$item = new template('menu.item');
+//kontrollime objekti olemasolu sisu
+echo '<pre>';
+print_r($menu);
+print_r($item);
+echo '</pre>';
+?>
