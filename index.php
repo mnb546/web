c539d4f3d7029c9a7bb1f82055ba1bdda2d9e4f0
mnb546 <rainer.lanemann@khk.ee>
2017-03-22 14:37:02 +0200

    malli elemendi ja väärtuse paarid index lehele

--- index.php
+++ index.php
@@ -12,6 +12,12 @@ echo '<h1>Veebipõhine esileht</h1> <h3>Rainer ISP14</h3>';
 
 // Valmistame peatemplate objekti
 $main_tmp = new template('main');
+//valmistame paarid malli_element => väärtus
+$main_tmp->set('user', 'Kasutajanimi');
+$main_tmp->set('title', 'Avaleht');
+$main_tmp->set('lang_bar', 'keeleriba');
+$main_tmp->set('menu', 'lehe_peamenüü');
+$main_tmp->set('content', 'lehe sisu');
 // Kontrollime antud objekti sisu
 echo '<pre>';
 print_r($main_tmp);
