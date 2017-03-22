1a902ba88e1723502ce7ee9c70a501a33b1feb76
mnb546 <rainer.lanemann@khk.ee>
2017-03-22 12:20:51 +0200

    Esmaversioon template failist

---
+++ classes/template.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rainer.lanemann
+ * Date: 22.03.2017
+ * Time: 12:11
+ */
+class template
+{
+    // template klassi omadused - muutujad
+    var $file = ''; // template fail
+    var $content = false; //html malli faili sisu
+    var $vars = array(); //html vaate sisu - reaalsed väärtused
+}
+
+?>
