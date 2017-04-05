d80d7fbae19c76db3297bbcc2cf60b31af332195
mnb546 <rainer.lanemann@khk.ee>
2017-04-05 08:47:24 +0300

    Acts kausta loomine ja kaks lehte (first; second) loomine selle sisse

---
+++ acts/first.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainer.lanemann
+ * Date: 5.04.2017
+ * Time: 8:44
+ */
+$main_tmp->set('content', 'Esimene leht');
+
+?>
---
+++ acts/second.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainer.lanemann
+ * Date: 5.04.2017
+ * Time: 8:46
+ */
+$main_tmp->set('content', 'Teine leht');
