c28aecb6643beec576c7a1da3833d4cfdfee7a7e
mnb546 <rainer.lanemann@khk.ee>
2017-04-12 11:00:00 +0300

    Automaatne avaleht andmebaasist

--- acts/default.php
+++ acts/default.php
@@ -5,5 +5,21 @@
  * Date: 5.04.2017
  * Time: 9:18
  */
-$main_tmp->set('content', 'Lehe sisu');
+// lehe id saamine ja teisendamine - andmebaasis BIGINT
+$page_id = $http->get('page_id');
+
+//sql lause lehe sisu itsimiseks vastavalt id-le
+$sql = 'SELECT * FROM content WHERE '.'content_id='.fixDb($page_id);
+// sadame päringu andmebaasi sisu saamiseks
+$res = $db->getArray($sql);
+if ($res === false) {
+    $sql = 'SELECT * FROM content WHERE '.'is_first_page='.fixDB(1);
+    $res = $db->getArray($sql);
+}
+if($res !== false) {
+    $page = $res[0];
+    $http->set('page_id', $page['content_id']);
+    $main_tmp->set('content', $page['content']);
+}
+
 ?>
